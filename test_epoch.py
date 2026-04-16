02acc6bfb471d67e9ab55c8c361872da5a1326b9
Boyan Iliev <boyan.georgiev.iliev@gmail.com>
2026-04-16 12:00:00 +0000

    test: cover detect unit microseconds

--- test_epoch.py
+++ test_epoch.py
@@ -10,3 +10,13 @@ def test_detect_unit_seconds():
 
 def test_detect_unit_milliseconds():
     assert epoch.detect_unit(1763000000000) == "ms"
+
+
+def test_detect_unit_microseconds():
+    assert epoch.detect_unit(1763000000000000) == "us"
+
+def test_from_timestamp_utc():
+    moment, unit = epoch.from_timestamp(0)
+    assert unit == "s"
+    assert moment.year == 1970
+    assert moment.tzinfo == timezone.utc
